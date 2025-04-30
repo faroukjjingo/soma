95886749a44dec3459257d095de0b38e1a9ea01b
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-30 14:12:19 +0300

    Update Navbar.jsx

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -51,29 +51,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
-</xaiArtefact>
 
-### src/components/common/Footer.jsx
-<xaiArtifact artifact_id="a09aa6fe-91af-47c4-a4ff-b6a49821d14e" artifact_version_id="dc689a56-f8df-4991-9a76-84b644c610a4" title="Footer.jsx" contentType="text/jsx">
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Soma. All rights reserved.</p>
-        <div className="mt-2 space-x-4">
-          <a href="/about" className="hover:text-gray-300">
-            About
-          </a>
-          <a href="/contact" className="hover:text-gray-300">
-            Contact
-          </a>
-          <a href="/privacy" className="hover:text-gray-300">
-            Privacy Policy
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
-};
 
-export default Footer;
