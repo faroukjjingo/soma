162674c8da5d168d400df84cc05c72dbfced1442
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-30 14:46:42 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CourseList from './pages/CourseList';
+import CourseDetailPage from './pages/CourseDetailPage';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import QuizPage from './pages/QuizPage';
+import Analytics from './pages/Analytics';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/courses" element={<CourseList />} />
+          <Route path="/courses/:id" element={<CourseDetailPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/quizzes/:id" element={<QuizPage />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
