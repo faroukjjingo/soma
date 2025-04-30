4f2f2c782a5aca80082c91bc569429ddfc0b92cb
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-30 15:53:03 +0300

    Create mailer.js

---
+++ backend/src/utils/mailer.js
@@ -0,0 +1,19 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async (to, subject, text) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  });
+};
