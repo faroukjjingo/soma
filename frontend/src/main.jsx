6f56389e20f8903658e5f333e7f8fb1aae8745d0
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-30 14:47:24 +0300

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+import './assets/styles/main.css';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
