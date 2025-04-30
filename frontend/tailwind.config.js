18c411092c966c5a7a9ab204ad91bcbe54d3ba19
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-30 17:18:29 +0300

    Update tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#9333EA',
+      },
+    },
+  },
+  plugins: [],
+};
