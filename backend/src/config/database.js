a74671d59ff3223aff18fa3d6825b95cce00e4fb
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-01 11:53:45 +0300

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI;
+    const uri = process.env.MONGO_URI;
     if (!uri) {
-      throw new Error('MONGODB_URI is not defined');
+      throw new Error('MONGO_URI is not defined');
     }
     await mongoose.connect(uri, {
       useNewUrlParser: true,
